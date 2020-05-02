88f57e1f221ed59c413c854a65c7522145ead5eb
Mark Liapustin <mliapustin@trustificorp.com>
2020-05-02 19:01:45 +0300

    Change 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,10 +7,10 @@ import TitleSection from 'components/ui/TitleSection';
 
 const NotFoundPage: React.FC = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="Page Not Found" />
     <Container section>
       <TitleSection title="404" subtitle="Page not found" center />
-      <p className="mt-4 text-center w-full">You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p className="mt-4 text-center w-full">Congratulations, you broke the page.</p>
     </Container>
   </Layout>
 );
