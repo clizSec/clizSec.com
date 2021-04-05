b1f35672c0d94ecfaa241a0747c2cbd7bfc7491b
Mark Liapustin <mliapustin@trustificorp.com>
2021-04-05 22:56:42 +0300

    Updated all styles and components to Gatsby v3.0

--- src/components/ContactInfo/styles.js
+++ src/components/ContactInfo/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
+// ${tw`w-full sm:w-1/2`}; <-- Backup for the style below
 export const ContactInfoItem = styled.div`
-  ${tw`w-full sm:w-1/2`};
+  ${tw`max-w-screen-sm mx-auto w-full px-0 sm:px-16 mb-4`};
 `;
